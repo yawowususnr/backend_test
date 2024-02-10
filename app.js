396e0836f92aaf4a8b5ec126155fa626260680e3
Yaw Owusu Snr <yawsnr@vt.edu>
2024-02-10 18:03:32 -0500

    removed public api

--- app.js
+++ app.js
@@ -24,9 +24,8 @@ const limiter = RateLimit({
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-const dev_db_url = "mongodb+srv://yawsnr:CclkNrDOuvDI2mby@cluster0.vbktfo1.mongodb.net/local_library?retryWrites=true&w=majority"
 
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI
 
 main().catch((err) => console.log(err));
 async function main() {
